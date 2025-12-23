0753d0212794f038e3ad625171f5d458d2c29576
aleksandrM <makalov.alex@yandex.ru>
2025-12-23 18:58:33 +0900

    реализация репозитория

--- internal/repository/file_repo.go
+++ internal/repository/file_repo.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
+	"archive/zip"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/AleksandrMac/fileserver/internal/domain"
 )
@@ -16,29 +18,85 @@ func NewFileRepository(storagePath string) *FileRepository {
 }
 
 func (x *FileRepository) GetFilePath(relPath string) string {
-	return ""
+	return filepath.Join(x.storagePath, relPath)
 }
 
 func (x *FileRepository) FileExists(path string) (bool, int64, error) {
-	return false, 0, nil
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, 0, nil
+	}
+	if err != nil {
+		return false, 0, err
+	}
+	return !info.IsDir(), info.Size(), nil
 }
 
 func (x *FileRepository) SaveFile(path string, data io.Reader) error {
-	return nil
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return err
+	}
+
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, data)
+
+	return err
 }
 
 func (x *FileRepository) ListZipContents(zipPath string) ([]domain.FileInfo, error) {
-	return nil, nil
+	zipReader, err := zip.OpenReader(zipPath)
+	if err != nil {
+		return nil, err
+	}
+	defer zipReader.Close()
+
+	var files []domain.FileInfo
+	for _, f := range zipReader.File {
+		if f.FileInfo().IsDir() {
+			continue
+		}
+
+		files = append(files, domain.FileInfo{
+			Name:    f.Name,
+			ModTime: f.Modified,
+		})
+	}
+
+	return files, nil
 }
 
 func (x *FileRepository) GetStorageSize() (int64, error) {
-	return 0, nil
+	var total int64
+
+	err := filepath.Walk(x.storagePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			total += info.Size()
+		}
+
+		return nil
+	})
+
+	return total, err
 }
 
 func (x *FileRepository) ReadFile(path string) (*os.File, error) {
-	return nil, nil
+	return os.Open(path)
 }
 
 func (x *FileRepository) GetFileSize(path string) (int64, error) {
-	return 0, nil
+	info, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	return info.Size(), nil
 }
